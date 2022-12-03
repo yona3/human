1ea39a109c02df558973741ba7f1997d03d1fe8a
yona3 <kou.y.0905@icloud.com>
2022-12-04 00:57:39 +0900

    ✨  feat(ui): あいさつ機能の実装

--- src/components/pages/home/Top/Message/index.tsx
+++ src/components/pages/home/Top/Message/index.tsx
@@ -1,14 +1,18 @@
 import { Box, Text } from "@mantine/core";
 import type { FC } from "react";
 
+import { useMessage } from "./hooks";
+
 /**
  * @package
  */
 export const Message: FC = () => {
+  const { message } = useMessage();
+
   return (
     <Box>
       <Text size="xs" align="center" color="dimmed">
-        おはようございます
+        {message}
       </Text>
     </Box>
   );
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -16,3 +16,19 @@ export const isDaytime = (() => {
   const h = new Date().getHours();
   return h >= 6 && h < 18;
 })();
+
+export const timeZone = (() => {
+  const h = new Date().getHours();
+
+  if (h >= 0 && h < 6) {
+    return "late night";
+  } else if (h >= 6 && h < 11) {
+    return "morning";
+  } else if (h >= 11 && h < 18) {
+    return "daytime";
+  } else if (h >= 18 && h < 24) {
+    return "night";
+  } else {
+    return "any";
+  }
+})();
