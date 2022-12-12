372bb71c1a21ff16f5580c207560ef39a3459d06
yona3 <kou.y.0905@icloud.com>
2022-12-13 00:55:52 +0900

    🐛  fix(app): ビルドエラーの修正

--- src/hooks/globalState/useMeState.ts
+++ src/hooks/globalState/useMeState.ts
@@ -2,9 +2,6 @@ import { meState } from "@/globalStates";
 
 import { useGlobalState } from "./useGlobalState";
 
-/**
- * @package
- */
 export const useMeState = () => {
   return useGlobalState(meState);
 };
